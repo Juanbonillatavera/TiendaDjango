90dbfd89aa966890a77ff30f62ca52c2e043753e
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-09-23 10:14:28 -0500

    cambios

--- Automotrizcarservice/appProductos/urls.py
+++ Automotrizcarservice/appProductos/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
     path('producto/', views.verProductos, name="productos"),
     path('producto/<str:id>', views.verProductos, name="producto"),
     path('carrito/<str:id>', views.agregar, name="carrito"),
+    path('vercarrito/', views.verCarrito, name="ver_carrito"),
+    path('eliminar/<str:id>', views.eliminarItemCarrito, name="eliminar"),
+     path('cambiarCantidad/', views.cambiarCantidad),
+    #funcion carrito visulizarlo
+    
 ]
--- Automotrizcarservice/appProductos/views.py
+++ Automotrizcarservice/appProductos/views.py
@@ -1,7 +1,13 @@
 from asyncio.windows_events import NULL
 from multiprocessing import context
+from operator import concat
 from django.shortcuts import render
-from .models  import Producto, Carrito
+from .models  import Producto, Carrito 
+
+from asyncio.windows_events import null
+from django.http import JsonResponse
+import json
+
 # Create your views here.
 
 
@@ -26,9 +32,9 @@ def agregar (request, id= NULL):
     id = int(id)
     user = request.user
     regProducto = Producto.objects.get(id=id)
-    existe= Carrito.objects.filter(producto = regProducto, estado ='carrito').exists()
+    existe= Carrito.objects.filter(cliente=user,producto = regProducto, estado ='carrito').exists()
     if existe:
-        regCarrito = Carrito.objects.get(producto = regProducto, estado ='carrito')
+        regCarrito = Carrito.objects.get(cliente=user,producto = regProducto, estado ='carrito')
         regCarrito.cantidad += 1
         regCarrito.save()
     else:
@@ -41,3 +47,72 @@ def agregar (request, id= NULL):
         }
     return render(request, 'productos/productos.html', context)
 
+
+
+
+
+def verCarrito(request):
+    regUser= request.user
+    carrito= Carrito.objects.filter(cliente=regUser, estado='carrito')
+
+    listaCarrito=[]
+    total= 0
+    for prod in carrito:
+
+        unProducto={
+        'cantidad': prod.cantidad,
+        'icono': prod.producto.icono,
+        'nombre': prod.producto.precio,
+        'unidad' : prod.producto.unidad,
+        'total' : int (prod.cantidad) * int(prod.producto.precio),
+        'prodId' : prod.producto.id,
+        'id' : prod.id,
+
+        }
+
+    listaCarrito.append(unProducto)
+    total += unProducto['total']
+
+    context={
+        'carrito': listaCarrito,
+        'subtotal': total,
+        'iva': total * 0.19,
+        'envio': 8000,
+        'total' : total * 1.19 + 8000,
+    }
+
+    return render(request,'productos/carrito.html', context)
+
+
+def eliminarItemCarrito(request,id):
+
+    regCarrito = Carrito.objects.get(id=id)
+    regCarrito.estado = 'cancelado'
+
+    regCarrito.save()
+
+    return verCarrito(request)
+
+
+def cambiarCantidad(request):
+    is_ajax = request.Meta.get('WTTP_X_REQUESTED_WITH')=='XMLHttpRequest'
+
+    if is_ajax:
+        if request.method == 'POST':
+            # TOMA LA DATA ENVIADA POR EL CLIENTE 
+            data = json.load(request)
+            id= data.get('id')
+            cantidad= int(data.get('cantidad'))
+            if cantidad > 0:
+                #lee el registro y lo modifica
+                regProducto = Carrito.objects.get(id=id)
+                regProducto.cantidad = cantidad
+                regProducto.save()
+            
+            return JsonResponse({'mensage': ' cantidad modificada a ' + str(cantidad)})
+
+        return JsonResponse({'alarma': 'no se pudo modificar....'}, status=400)
+
+
+    else:
+        return verCarrito(request)
